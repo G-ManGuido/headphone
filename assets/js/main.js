bd0fdabd96b69c517280890c4889068f87e9be4e
G-ManGuido <supercoder@AirvonSperCoder.fritz.box>
2022-01-30 11:41:29 +0100

    mit Video

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+let push = document.getElementById("push");
+let deinname = document.getElementById("deinname");
+let email = document.getElementById("email");
+let country = document.getElementById("country");
+let comment = document.getElementById("comment");
+let mailinglist = document.getElementById("mailinglist");
+let robot = document.getElementById("robot");
+
+
+
+function letsrock() {
+    console.log("Name: " + deinname.value);
+    console.log("Email: " + email.value);
+    console.log("Land: " + country.value);
+    console.log("Kommentar: " + comment.value);
+    console.log("Newsletter: " + mailinglist.value);
+    console.log("robot: " + robot.value);
+}
